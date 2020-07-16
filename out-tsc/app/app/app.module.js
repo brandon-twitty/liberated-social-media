820a9df2a4b2d16fc709896fa7e397064fe6144c
Twitty <btwitty187@gmail.com>
2020-07-16 15:55:49 -0500

    started fresh with out amplify

---
+++ out-tsc/app/app/app.module.js
@@ -0,0 +1,26 @@
+import { __decorate } from "tslib";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+let AppModule = class AppModule {
+};
+AppModule = __decorate([
+    NgModule({
+        declarations: [AppComponent],
+        entryComponents: [],
+        imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+        providers: [
+            StatusBar,
+            SplashScreen,
+            { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+        ],
+        bootstrap: [AppComponent]
+    })
+], AppModule);
+export { AppModule };
+//# sourceMappingURL=app.module.js.map
