1e2469071b5d170833e13b17ec575f26046aed00
brandon-twitty <btwitty187@gmil.com>
2020-07-30 16:30:16 -0500

    added create profile

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -8,14 +8,17 @@ import { ProfilePageRoutingModule } from './profile-routing.module';
 
 import { ProfilePage } from './profile.page';
 import {CreateComponent} from './create/create.component';
+import {HeaderModule} from '../header/header.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    ProfilePageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        IonicModule,
+        ProfilePageRoutingModule,
+        HeaderModule
+    ],
   declarations: [ProfilePage, CreateComponent]
 })
 export class ProfilePageModule {}
