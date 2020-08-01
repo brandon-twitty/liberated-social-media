158f361f3866b161d1577061b3e7bc1eabf8cb76
Twitty <btwitty187@gmail.com>
2020-08-01 14:30:18 -0500

    toolbar working

---
+++ src/app/all-users/all-users.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-users',
+  templateUrl: './all-users.page.html',
+  styleUrls: ['./all-users.page.scss'],
+})
+export class AllUsersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
